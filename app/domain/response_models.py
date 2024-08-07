730f184e75384d3db5bd238e27d29c6d490aa25b
moracabanas <moracabanas@live.com>
2024-08-07 18:34:42 +0200

    Add new models

--- app/domain/response_models.py
+++ app/domain/response_models.py
@@ -1,15 +1,20 @@
 from app.domain.models import *
 
 
-class VanoPredictionResponse(BaseModel):
-    ID_VANO: str
-    PARAMETROS: dict
+class VanoPrediction(BaseModel):
+    PARAMETROS_a_h_k: dict
     RECONSTRUCION: str
     CONDUCTORES_CORREGIDOS: dict
     CONDUCTORES_CORREGIDOS_PARAMETROS: dict
     FLAG: str
     NUM_CONDUCTORES: int
+    NUM_CONDUCTORES_FIABLES: bool
     CONFIG_CONDUCTORES: int
     COMPLETITUD: str
     ERROR_POLILINEA: float
     ERROR_CATENARIA: float
+
+
+class VanoPredictionResponseSchema(BaseModel):
+    ID_VANO: str
+    PREDICCION: VanoPrediction
