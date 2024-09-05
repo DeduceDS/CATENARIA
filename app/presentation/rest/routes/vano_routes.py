5302817902e8004e6950e1a4cf2ad29980167a0d
moracabanas <moracabanas@live.com>
2024-09-05 13:57:23 +0200

    Update

--- app/presentation/rest/routes/vano_routes.py
+++ app/presentation/rest/routes/vano_routes.py
@@ -41,21 +41,26 @@ async def predict_vano_handler(
 
 # Database vano operations
 if settings.DATABASE_FEATURE:
-
+    # Upload vanos to DB by Linea json file
     @vano_database_router.post("/upload_linea_file")
     async def upload_file_handler(
         file: UploadFile = File(...), db: AsyncSession = Depends(get_db)
     ):
         # FIXME this service isn't even triggering when database.db file gives 500 error due to UTF8 encoding error on UploadFile
         file_checker_service = FileCheckerServiceImpl()
+        vano_repository = VanoRepositoryImpl(db)
+        electra_service = LineaDataServiceImpl(vano_repository)
+
         file_checker_service.validate_file_type(file, "json")
 
+        content = await file.read()  # file to bytes
+        data = json.loads(content)  # bytes to dict
+
         # Convert the list of dictionaries to a list of Vano objects
+        # TODO is this line necessary?
         vanos = [Vano(**vano_data) for vano_data in data]
         linea = Linea(vanos=vanos)
 
-        vano_repository = VanoRepositoryImpl(db)
-        electra_service = LineaDataServiceImpl(vano_repository)
         await electra_service.save_linea(linea)
 
         return {"message": "File processed successfully"}
@@ -81,6 +86,19 @@ if settings.DATABASE_FEATURE:
 
     @vano_database_router.get("/vanos/{id}")
     async def get_vano_by_id_handler(
+        id: str, db: AsyncSession = Depends(get_db)
+    ) -> Vano:
         vano_repository = VanoRepositoryImpl(db)
         vano = await vano_repository.get_vano(id)
         return vano
+
+    @vano_database_router.get("/vanos/predict/{id}")
+    async def predict_vano_by_id_handler(
+        id: str, db: AsyncSession = Depends(get_db)
+    ) -> VanoPrediction:
+        vano_repository = VanoRepositoryImpl(db)
+        vano = await vano_repository.get_vano(id)
+
+        prediction_service = LineaPredictServiceImpl()
+        prediction = await prediction_service.predict_vano(vano)
+        return prediction
